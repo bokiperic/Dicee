8e54a92e1953f840ba8cac4b6bfc4235a715e2f6
Bojan Peric <b.peric@levi9.com>
2019-05-26 13:27:26 +0200

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Bojan Peric on 5/26/19.
+//  Copyright © 2019 Boki Peric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
